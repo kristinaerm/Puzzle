e857174f254ec545b620e8a5b73098013dd31fcf
glotovapolina <33525395+glotovapolina@users.noreply.github.com>
2018-11-12 21:49:42 +0400

    кк

--- Puzzle/Puzzle/GameOnField.cs
+++ Puzzle/Puzzle/GameOnField.cs
@@ -33,30 +33,8 @@ namespace Puzzle
             string path = bd.SelectPathPicture(id_picture);
             List<string> picture = bd.SelectInPuzzle(id_puzzle);
             var btm = new List<Bitmap>();
-<<<<<<< HEAD
-            Image img = System.Drawing.Image.FromFile(path);
-            btm = Section.RectangleSection(path, picture[0], picture[1], picture[2], id_picture);//разрезаем картинку на кусочки
-            List<PictureBox> pb = new List<PictureBox>();//создаем список пикчербоксов
-            for (int i = 0; i < btm.Count; i++)
-            {
-                PictureBox a = new PictureBox();
-                a.Height = btm[i].Height;
-                a.Width = btm[i].Width;
-                Point xy = new Point(12+(i* btm[i].Width+1), 27 + (i * btm[i].Height+1));
-                a.Location = xy;
-                a.SizeMode = PictureBoxSizeMode.StretchImage;
-                        a.Image = btm[0];  
-                flowLayoutPanel1.Controls.Add(a);
-                pb.Add(a);
-                //pb.Add(new PictureBox());
-                //pb[i].Image = btm[i];
 
-                // pictureBox1.Image = pb[i].Image;
-            }
-            //pictureBox1.Image = pb[1].Image;
-            //тут резать выволить и тд
-          
-=======
+            Image img = System.Drawing.Image.FromFile(path);
             btm=Section.RectangleSection(path, picture[0], picture[1], picture[2], id_picture);//разрезаем картинку на кусочки
             List<PictureBox> pb = new List<PictureBox>();//создаем массив пикчербоксов
             int h = btm[0].Height;
@@ -88,7 +66,7 @@ namespace Puzzle
                 pb[i].Image = btm[i];
                 flowLayoutPanel1.Controls.Add(p);
             }
->>>>>>> 15d667cab3ddcfa72de181025217b23805b4f292
+
         }
 
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
