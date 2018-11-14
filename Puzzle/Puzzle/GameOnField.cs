bcdda259ec6b9495a0474b1736134f51319cb21f
kristinaerm <32340053+kristinaerm@users.noreply.github.com>
2018-11-14 19:32:59 +0400

    фы

--- Puzzle/Puzzle/GameOnField.cs
+++ Puzzle/Puzzle/GameOnField.cs
@@ -25,8 +25,8 @@ namespace Puzzle
         int verticalCountOfPieces = 0;
         int horisontalCountOfPieces = 0;
         PictureBox hint;
+        private static Random rng = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
 
-       
         //***********
         public GameOnField(string id_puzzle, string game_mode, string record)
         {
@@ -42,9 +42,12 @@ namespace Puzzle
             List<string> picture = bd.SelectInPuzzle(id_puzzle);
             verticalCountOfPieces = Convert.ToInt32(picture[0]);
             horisontalCountOfPieces = Convert.ToInt32(picture[1]);
-            var btm = new List<Bitmap>();
+            var btm = new List<Bitmap>();//нормальный список кусочков пазл
+            var btm1 = new List<Bitmap>();//перемешанный список
             Image img = System.Drawing.Image.FromFile(path);
             btm = Section.RectangleSection(path, picture[0], picture[1], picture[2], id_picture);//разрезаем картинку на кусочки
+            btm1 = Section.RectangleSection(path, picture[0], picture[1], picture[2], id_picture);//разрезаем картинку на кусочки
+            Shuffle<Bitmap>(btm1);//перемешиваем кусочки списка
             List<PictureBox> pb = new List<PictureBox>();//создаем массив пикчербоксов
             int h = btm[0].Height;
             int w = btm[0].Width;
@@ -83,9 +86,9 @@ namespace Puzzle
                     //    currh += h + 5;
                     //}
                     p.SizeMode = PictureBoxSizeMode.StretchImage;
-                    p.Image = (Image)btm[i];
+                    p.Image = (Image)btm1[i];
                     pb.Add(p);
-                    pb[i].Image = btm[i];
+                    pb[i].Image = btm1[i];
                     this.Controls.Add(p);
                     
                     ControlMover.Add(pb[i]); //перемещение кусочков
@@ -123,8 +126,20 @@ namespace Puzzle
             //hint.BringToFront();
 
         }
-     
-
+  
+            //метод, который рандомит элементы списка
+        public static void Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
