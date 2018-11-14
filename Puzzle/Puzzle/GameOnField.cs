d3afe9cdda470c87ad3a2ecdaf3a46525c76657b
kristinaerm <32340053+kristinaerm@users.noreply.github.com>
2018-11-14 15:01:39 +0400

    213

--- Puzzle/Puzzle/GameOnField.cs
+++ Puzzle/Puzzle/GameOnField.cs
@@ -20,6 +20,10 @@ namespace Puzzle
         string id_puzzle = "";
         string game_mode = "";
         string record = "";
+        int verticalCountOfPieces = 0;
+        int horisontalCountOfPieces = 0;
+        PictureBox hint;
+
         private Point MouseDownLocation;
         //***********
         public GameOnField(string id_puzzle, string game_mode, string record)
@@ -29,9 +33,12 @@ namespace Puzzle
             this.id_puzzle = id_puzzle;
             this.game_mode = game_mode;
             this.record = record;
+            
             string id_picture = bd.selectIdPicture(id_puzzle);
             string path = bd.SelectPathPicture(id_picture);
             List<string> picture = bd.SelectInPuzzle(id_puzzle);
+            verticalCountOfPieces = Convert.ToInt32(picture[0]);
+            horisontalCountOfPieces = Convert.ToInt32(picture[1]);
             var btm = new List<Bitmap>();
             Image img = System.Drawing.Image.FromFile(path);
             if(game_mode=="На поле")
@@ -47,7 +54,19 @@ namespace Puzzle
             int count = btm.Count;
             int countW = Int32.Parse(picture[0]);
             int countH = Int32.Parse(picture[1]);
+<<<<<<< HEAD
                 for (int i = 0; i < count; i++)
+=======
+            for (int i = 0; i < count; i++)
+            {
+                int W = currW;
+                int H = currH;
+                PictureBox p = new PictureBox();
+                p.Location = new Point(W * (w+1) + 5, H * (h +1)+25);
+                p.Size = new Size(w, h);
+                currW++;
+                if (currW == countW)
+>>>>>>> 4f7184ef808bf7535daa316c5630d0a005509416
                 {
                     int W = currW;
                     int H = currH;
@@ -87,8 +106,15 @@ namespace Puzzle
                 if(game_mode=="На ленте") {
 
             }
+            hint = new PictureBox();
+            hint.SizeMode = PictureBoxSizeMode.StretchImage;
+            hint.Size = new Size((w+1)* verticalCountOfPieces, (h+1)* horisontalCountOfPieces);
+            hint.Location = new Point(5,25);
+            hint.Image = Image.FromFile(path);
+            this.Controls.Add(hint);
+            hint.Visible = false;
+            hint.BringToFront();
 
-          
         }
 
        
@@ -122,5 +148,30 @@ namespace Puzzle
         {
 
         }
+
+        private void button_help_Click(object sender, EventArgs e)
+        {
+            if (hint.Visible)
+            {
+                button_help.Text = "Показать подсказку";
+            }
+            else
+            {
+                button_help.Text = "Скрыть подсказку";
+            }
+            hint.Visible = !hint.Visible;
+        }
+
+        private void GameOnField_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Точно хотите выйти? Для сохранения игры нажмите Отмена и Сохранить", "Уведомление", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
     }
+
+
 }
+
